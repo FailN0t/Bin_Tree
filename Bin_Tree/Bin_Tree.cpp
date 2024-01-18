56290848455456db87bcc72e04c3edec0e30901f
Igor Putyajkin <putyajkin99@gmail.com>
2024-01-18 12:58:24 +0300

    штш45

--- Bin_Tree/Bin_Tree.cpp
+++ Bin_Tree/Bin_Tree.cpp
@@ -1,209 +1,77 @@
-﻿#include <windows.h>
-#include <iostream>
-#include <stdio.h>
-#include <io.h>
-#include <stdlib.h>
-#include <time.h>
-#include <sys\locking.h>
-#include <string.h>
-#include <ctype.h>
-using namespace std;
-//Максимальная длина слова
-#define MAX_WORD_LENGTH 21
-//Кол-во попыток
-int Tries = 10;
-//Кол-во угаданных слов
-int CountWords = 0;
-//Загрузка слова
-bool LoadWord(FILE * file, char* word)
-{
-	int i = 0;
-	char s[MAX_WORD_LENGTH] = { 0 };
-	//Кол-во слов в файле
-	static int count = -1;
-	if (count == -1)
-	{
-		//Подсчет количества слов
-		while (!feof(file))
-		{
-			fgets(s, MAX_WORD_LENGTH, file);
-			count++;
-		}
-		//Слов нет?
-		if (count == 0)
-			return false;
-		//Возврат файлового указателя в начало файла
-		fseek(file, 0, 0);
-	}
-	//Случайное слово
-	int n = rand() % count;
-	//Поиск слова
-	while (i <= n)
-	{
-		fgets(s, MAX_WORD_LENGTH, file);
-		i++;
-	}
-	//Определяем длину слова
-	int wordlen = strlen(s);
-	//Минимальная длина слова 2 буквы
-	if (wordlen <= 1)
-		return false;
-		//Убираем Enter (в DOS'е 2 байта 13 10)
-		if (s[wordlen - 1] == 10)
-			s[wordlen - 2] = 0;
-		else if (s[wordlen - 1] == 13)
-			s[wordlen - 1] = 0;
-	//Копируем слово
-	strcpy(word,s);
-	//Получаем дескриптор файла
-	int hFile = _fileno(file);
-	//Вычисляем размер файла
-	int size = _filelength(hFile);
+﻿// Bin_Tree.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
 
-	//Блокировка файла
-	fseek(file, 0, 0);
-	_locking(hFile, _LK_NBLCK, size);
-	return true;
-}
-//Игра
-void Game(char* word)
+#include <iostream>
+using namespace std;
+
+struct Node
 {
-	//Перевод в большие буквы
-	strupr(word);
-	int len = strlen(word);
-	//Строка-копия
-	char* copy = new char[len + 1];
-	memset(copy, '*', len);
-	copy[len] = 0;
-	//Алфавит + пробелы
-	char letters[52];
-	int i, j = 0;
-	for (i = 0; i < 26; i++)
-	{
-		letters[j++] = i + 'A';
-			letters[j++] = ' ';
-	}
-	//Замыкающий ноль
-	letters[51] = 0;
-	//Буква
-	char letter;
-	char* pos;
-	bool replace = false;
-	do {
-		//Очистка экрана
-		system("cls");
-		cout << copy << endl << endl;
-		cout << letters << endl << endl;
-		cout << "Count of tries: " << Tries << endl
-			<< endl;
-		cout << "Input any letter:\t";
-		cin >> letter;
-		//Звуковой сигнал
-		Beep(500, 200);
-		//if(letter >= 'A' && letter <= 'Z'
-		//|| letter >= 'a' && letter <= 'z')
-		//Буква?
-		if (!isalpha(letter))
-		{
-			cout << "It's not a letter" << endl;
-			//Задержка на 1 секунду
-			Sleep(1000);
-			continue;
-		}
-		//Перевод буквы в большую
-		letter = toupper(letter);
-			//Поиск буквы в алфавите
-			pos = strchr(letters, letter);
-		//Такая буква уже была
-		if (pos == 0)
-		{
-			cout << "This letter have been already pressed" << endl;
-				Sleep(1000);
-			continue;
-		}
-		else
-		{
-			//Убираем букву из алфавита
-			pos[0] = ' ';
-		}
-		//Поиск буквы в слове
-		for (i = 0; i < len; i++)
-		{
-			if (word[i] == letter)
-			{
-				copy[i] = letter;
-				replace = true;
-			}
-		}
-		if (replace == false)
-			Tries--;
-		else
-			replace = false;
-		//Условие победы
-		if (strcmp(word, copy) == 0)
-		{
-			system("cls");
-			cout << copy << endl << endl;
-			cout << letters << endl << endl;
-				cout << "Count of tries: " << Tries <<
-				endl << endl;
-			cout << "Congratulation !!!" << endl;
-			CountWords++;
-			break;
-		}
-	} while (Tries != 0);
-	delete[] copy;
-}
-void main()
+    Node* head = NULL;
+    int data;
+    Node* left = NULL;
+    Node* right = NULL;
+};
+
+class Bin_Tree {
+    Node* node;
+    int size = 0;
+public:
+    Node* create(Node* head, Node*& nodes, int in, int& j) {
+
+        if (in <= 0) { return NULL; }
+        nodes = new Node;
+        nodes->head = head;
+        nodes->data = ++j;
+        in--;
+        create(nodes, nodes->left, in, j);
+        create(nodes, nodes->right, in, j);
+
+
+        return nodes;
+    }
+    Bin_Tree(int in) {
+        create(NULL, node, in, size);
+    }
+
+    Node* getNode() {
+        return node;
+    }
+
+    void print(ostream& ost, Node* node) {
+        ost << node->data << endl;
+        if (node->right != NULL) {
+            print(ost, node->right);
+        }
+        if (node->left != NULL) {
+            print(ost, node->left);
+        }
+
+    }
+    friend ostream& operator<< (ostream& ost, Bin_Tree& bt) {
+        bt.print(ost, bt.node);
+        return ost;
+    }
+
+    void add(int data) {
+
+    }
+
+};
+
+int main()
 {
-	//Открываем файл на чтение в двоичном режиме
-	FILE* f = fopen("words.txt", "rb");
-	//Если файл не открылся
-	if (f == 0)
-	{
-		//Ошибка
-		perror("Open");
-		return;
-	}
-	srand(time(0));
-	char Word[20];
-	//Пытаемся загрузить слово
-	if (!LoadWord(f, Word))
-	{
-		//Если неудачно
-		cout << "Error !!!" << endl;
-		fclose(f);
-		return;
-	}
-	char answer;
-	//Играем, пока не надоест
-	do
-	{
-			Game(Word);
-		//Если попыток не осталось, то выход
-		if (Tries == 0)
-		{
-			cout << "Count of words: " << CountWords << endl;
-			cout << "Bye-bye" << endl;
-			break;
-		}
-		//Если остались
-		cout << "Continue ??? (Y/N)\t";
-		cin >> answer;
-		//Еще играем?
-		if (answer == 'Y' || answer == 'y')
-			if (!LoadWord(f, Word))
-			{
-				cout << "Error !!!" << endl;
-				fclose(f);
-				return;
-			}
-	} while (answer == 'Y' || answer == 'y');
-	//получаем дескриптор
-	int hFile = _fileno(f);
-	//Разблокировка файла
-	int size = _filelength(hFile);
-	fseek(f, 0, 0);
-	_locking(hFile, _LK_UNLCK, size);
-	fclose(f);
+    Bin_Tree BT(3);
+    cout << BT;
+
 }
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
